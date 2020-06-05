d0257bd4c2e2159cb18703df66b9ab1f8d6590af
felixheli <helimc.minecraft@gmail.com>
2020-06-05 15:54:01 +0200

    smol bug fix

--- mysql/query.js
+++ mysql/query.js
@@ -1,10 +1,6 @@
 module.exports = (query, placeholders, router) => {
-    
     const connection = require("./connect")();
-
     connection.query(query, placeholders, (error, result, fields) => {
-        console.log(result)
-        
-        //router.handler(error, result, fields, router);
+        router.handler(error, result, fields, router);
     });
 }
