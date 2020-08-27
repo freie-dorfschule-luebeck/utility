35a128ff17cf4bf76f637683dc352f62fb765072
Felix Johannes Caldeira Cabral <46715718+twentythreetea@users.noreply.github.com>
2020-08-27 16:26:02 +0200

    Update index.js

--- string/index.js
+++ string/index.js
@@ -1,5 +1,6 @@
 String.isEmail = require("./isEmail");
 String.isPassword = require("./isPassword");
+String.isPasswordValidator = require("./isPasswordValidator");
 String.isNick = require("./isNick");
 module.exports = String;
 
