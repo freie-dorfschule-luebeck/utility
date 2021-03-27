245722581cbc679fdb2149456fb9dd69ee3faab7
Felix Johannes Caldeira Cabral <46715718+twentythreetea@users.noreply.github.com>
2021-03-27 05:45:48 +0100

    Update isPassword.js

--- string/isPassword.js
+++ string/isPassword.js
@@ -2,10 +2,10 @@ const errorHandling = require("../errors");
 const cfg = require("./config.json").password;
 
 module.exports = (password) => {
-    if(!password) throw new errorHandling.SutekinaError("No password was entered", 400)
-    if(password.length < cfg.minimum) throw new errorHandling.SutekinaError("The entered password is too short!", 400);
-    if(password.length > cfg.maximum) throw new errorHandling.SutekinaError("The entered password is too long!", 400);
+    if(!password) throw new errorHandling.SutekinaError("NO_PASSWORD", 400)
+    if(password.length < cfg.minimum) throw new errorHandling.SutekinaError("PASSWORD_TOO_SHORT", 400);
+    if(password.length > cfg.maximum) throw new errorHandling.SutekinaError("PASSWORD_TOO_LONG", 400);
     const regexp = new RegExp(`[${cfg.forbidden_characters.join("")}]`);
-    if(regexp.test(password)) throw new errorHandling.SutekinaError("The entered password includes a forbidden character!", 400);
+    if(regexp.test(password)) throw new errorHandling.SutekinaError("PASSWORD_INVALID", 400);
     return true;
 }
