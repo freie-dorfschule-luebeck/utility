5e268b17728bcf464033a4da5359f32ce21e65d7
Felix Johannes Caldeira Cabral <46715718+twentythreetea@users.noreply.github.com>
2021-04-03 19:21:41 +0200

    better

--- string/isNick.js
+++ string/isNick.js
@@ -2,10 +2,10 @@ const errorHandling = require("../errors");
 const cfg = require("./config.json").nick;
 
 module.exports = (nick) => {
-    if(!nick) throw new errorHandling.SutekinaError("No nickname entered", 400)
-    if(nick.length < cfg.minimum) throw new errorHandling.SutekinaError("Nickname is too short!", 400);
-    if(nick.length > cfg.maximum) throw new errorHandling.SutekinaError("Nickname is too long!", 400);
+    if(!nick) throw new errorHandling.SutekinaError("NO_NICK", 400)
+    if(nick.length < cfg.minimum) throw new errorHandling.SutekinaError("NICK_TOO_SHORT", 400);
+    if(nick.length > cfg.maximum) throw new errorHandling.SutekinaError("NICK_TOO_LONG", 400);
     const regexp = new RegExp(`[^${cfg.allowed_characters.join("")}]`);
-    if(regexp.test(nick)) throw new errorHandling.SutekinaError("Nickname can only include alphanumerical characters, spaces or dots!", 400);
+    if(regexp.test(nick)) throw new errorHandling.SutekinaError("NICK_INVALID", 400);
     return true;
 }
